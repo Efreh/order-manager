49195560ed9191fda4a170ef0065dd72b34dcc38
Alexandr Ludov <efraam@gmail.com>
2024-06-02 02:05:53 +0600

    add spring sequrity

---
+++ src/main/java/com/efreh/order_manager/configuration/SecurityConfig.java
@@ -0,0 +1,38 @@
+package com.efreh.order_manager.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http.authorizeHttpRequests(authorizeHttpRequests ->
+                        authorizeHttpRequests
+                                .anyRequest().authenticated()
+                )
+                .formLogin(Customizer.withDefaults())
+                .httpBasic(Customizer.withDefaults());
+        return http.build();
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService(){
+        PasswordEncoder encoder = PasswordEncoderFactories.createDelegatingPasswordEncoder();
+        UserDetails user = User.builder()
+                .username("test")
+                .password(encoder.encode("test"))
+                .roles("USER")
+                .build();
+        return new InMemoryUserDetailsManager(user);
+    }
+}
--- src/main/java/com/efreh/order_manager/entity/Employee.java
+++ src/main/java/com/efreh/order_manager/entity/Employee.java
@@ -9,21 +9,21 @@ public class Employee {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "employeeId")
     private int employeeId;
-    @Column(name = "login_phone",unique = true,nullable = false,length = 20)
+    @Column(name = "login_phone", unique = true, nullable = false, length = 20)
     private String login_phone;
-    @Column(name = "name",nullable = false,length = 20)
+    @Column(name = "name", nullable = false, length = 20)
     private String name;
-    @Column(name = "otchestvo",length = 20)
+    @Column(name = "otchestvo", length = 20)
     private String otchestvo;
-    @Column(name = "surname",nullable = false,length = 20)
+    @Column(name = "surname", nullable = false, length = 20)
     private String surname;
-    @Column(name = "department",nullable = false,length = 20)
+    @Column(name = "department", nullable = false, length = 20)
     private String department;
-    @Column(name = "sector",nullable = false,length = 20)
+    @Column(name = "sector", nullable = false, length = 20)
     private String sector;
-    @Column(name = "work_center",length = 20)
+    @Column(name = "work_center", length = 20)
     private String work_center;
-    @Column(name = "job_title",length = 20)
+    @Column(name = "job_title", length = 20)
     private String job_title;
 
     public Employee() {
