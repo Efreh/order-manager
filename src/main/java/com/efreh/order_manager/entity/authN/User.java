3a60b5938f7002fab4fede31f68e521a57abd949
Alexandr Ludov <efraam@gmail.com>
2024-06-11 23:56:10 +0600

    fix authorization
    Corrected user table for spring security to work correctly. Depending on the different roles, the contents of the /home page display different elements.

--- src/main/java/com/efreh/order_manager/entity/authN/User.java
+++ src/main/java/com/efreh/order_manager/entity/authN/User.java
@@ -11,7 +11,7 @@ import java.util.Collection;
 import java.util.stream.Collectors;
 
 @Entity
-@Table(name = "users")
+@Table(name = "user")
 public class User implements UserDetails {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
